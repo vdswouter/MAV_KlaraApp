0fbfa39d11616f33397ee8b195a8bebed7b3c48c
Wouter <wouter.vandersyppe@gmail.com>
2014-01-27 16:22:55 +0100

    responsive design + qualitypicker bugfix

--- Klara Overal/Classes/Controllers/LivestreamViewController.m
+++ Klara Overal/Classes/Controllers/LivestreamViewController.m
@@ -20,8 +20,9 @@
     if (self) {
         // Custom initialization
         self.currentAudioQuality = @"MID";
+        self.firstTime = YES;
         
-        self.klaraStreamURLs = [[NSDictionary alloc] initWithObjectsAndKeys:@"http://mp3.streampower.be/stubru-high.mp3",@"hoog",@"http://mp3.streampower.be/klara-mid.mp3",@"MID",@"http://mp3.streampower.be/stubru-low.mp3",@"LOW",nil];
+        self.klaraStreamURLs = [[NSDictionary alloc] initWithObjectsAndKeys:@"http://mp3.streampower.be/klara-high.mp3",@"HIGH",@"http://mp3.streampower.be/klara-mid.mp3",@"MID",@"http://mp3.streampower.be/klara-low.mp3",@"LOW",nil];
 
         self.streamer = [[STKAudioPlayer alloc] init];
     }
@@ -33,7 +34,13 @@
     [self.view.btnPlay addTarget:self action:@selector(startStream:) forControlEvents:UIControlEventTouchUpInside];
     [self.view.btnPause addTarget:self action:@selector(stopStream:) forControlEvents:UIControlEventTouchUpInside];
     
-    self.qualityPicker = [[QualityPicker alloc] initWithFrame:CGRectMake(17, 450, 285, 34)];
+    CGRect qualityPickerFrame;
+    if ([self hasFourInchDisplay]) {
+        qualityPickerFrame = CGRectMake(17, 467, 285, 34);
+    }else{
+        qualityPickerFrame = CGRectMake(17, 440, 285, 34);
+    }
+    self.qualityPicker = [[QualityPicker alloc] initWithFrame:qualityPickerFrame];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(qualityChangedHandler:) name:@"QUALITY_CHANGED" object:self.qualityPicker];
     [self.view addSubview:self.qualityPicker];
 }
@@ -45,24 +52,39 @@
 }
 
 -(void)startStream:(id)sender{
+    self.view.btnPlay.userInteractionEnabled = NO;
+    self.view.btnPause.userInteractionEnabled = YES;
     [self.streamer play:[self.klaraStreamURLs objectForKey:self.currentAudioQuality]];
     [UIView animateWithDuration:0.3 animations:^{
-        self.qualityPicker.frame = CGRectMake(17, 386, 285, 34);
-        self.qualityPicker.btnMid.titleLabel.textColor = [UIColor whiteColor];
-        self.qualityPicker.btnMid.backgroundColor = [UIColor blackColor];
-        self.qualityPicker.btnMid.userInteractionEnabled = false;
-        self.view.btnPlay.frame = CGRectMake(CGRectGetMinX(self.view.btnPlay.frame), CGRectGetMinY(self.view.btnPlay.frame), 90, 40);
-        self.view.btnPause.frame = CGRectMake(CGRectGetMinX(self.view.btnPause.frame), CGRectGetMinY(self.view.btnPause.frame), 90, 40);
+        if (![self hasFourInchDisplay]) {
+            self.qualityPicker.frame = CGRectMake(17, 386, 285, 34);
+            self.view.btnPlay.frame = CGRectMake(CGRectGetMinX(self.view.btnPlay.frame), CGRectGetMinY(self.view.btnPlay.frame), 90, 40);
+            self.view.btnPause.frame = CGRectMake(CGRectGetMinX(self.view.btnPause.frame), CGRectGetMinY(self.view.btnPause.frame), 90, 40);
+        }
+        if (self.firstTime) {
+            self.qualityPicker.btnMid.titleLabel.textColor = [UIColor whiteColor];
+            self.qualityPicker.btnMid.backgroundColor = [UIColor blackColor];
+            self.qualityPicker.btnMid.userInteractionEnabled = false;
+            self.firstTime = NO;
+        }
     }];
 }
 
 -(void)stopStream:(id)sender{
+    self.view.btnPause.userInteractionEnabled = NO;
+    self.view.btnPlay.userInteractionEnabled = YES;
     [self.streamer stop];
-    [UIView animateWithDuration:0.3 animations:^{
-        self.qualityPicker.frame = CGRectMake(17, 450, 285, 34);
-        self.view.btnPlay.frame = CGRectMake(CGRectGetMinX(self.view.btnPlay.frame), CGRectGetMinY(self.view.btnPlay.frame), 90, 90);
-        self.view.btnPause.frame = CGRectMake(CGRectGetMinX(self.view.btnPause.frame), CGRectGetMinY(self.view.btnPause.frame), 90, 90);
-    }];
+    if (![self hasFourInchDisplay]) {
+        [UIView animateWithDuration:0.3 animations:^{
+            self.qualityPicker.frame = CGRectMake(17, 440, 285, 34);
+            self.view.btnPlay.frame = CGRectMake(CGRectGetMinX(self.view.btnPlay.frame), CGRectGetMinY(self.view.btnPlay.frame), 90, 90);
+            self.view.btnPause.frame = CGRectMake(CGRectGetMinX(self.view.btnPause.frame), CGRectGetMinY(self.view.btnPause.frame), 90, 90);
+        }];
+    }
+}
+
+- (BOOL)hasFourInchDisplay {
+    return ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone && [UIScreen mainScreen].bounds.size.height == 568.0);
 }
 
 - (void)viewDidLoad
